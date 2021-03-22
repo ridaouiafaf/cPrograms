efc4f0ef20035926e0c13ebae90e67d23fecf046
ridaouiafaf <afafridaoui@gmail.com>
2021-03-22 08:00:37 +0100

    added comments & fixing bug

--- Chapter 1 - A tutorial introduction/Line Counting/exercise_1_9.c
+++ Chapter 1 - A tutorial introduction/Line Counting/exercise_1_9.c
@@ -7,27 +7,31 @@ int main()
 {
 
 	int c[50], ch;
+	// table to store characters & a variable to read character by character.
 
-	for (int i = 1; ch != EOF; i++)
+	for (int i = 0; ch != EOF; i++)
 	{
 		ch = getchar();
 
 		if (i==0)
 		{
-			c[i]= ch;
+			c[i]= ch;// when we are at the first element, which is c[0], 
+			//we can't do the upcoming steps so we just assign directly ch value.
 		}
 		else
 		{
 			if (c[i - 1] != ' ')
 			{
-				c[i] = ch;
+				c[i] = ch; 
+				/*We test if the previous element is not a blank, if so, we add the current element no matter what it is*/
 			}
 			else
 			{
-				i++;
+				continue;
+				// if not, which means the previous element is a blank, we don't add it and we continue.
 			}
 		}
-		putchar(c[i]);
+		putchar(c[i]);// we write each character in time!
 	}
 
 	return 0;
